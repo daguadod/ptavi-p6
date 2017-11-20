7f2f696258284f5254a702cfb0a4d4927320842f
David Aguado Dianez <daguadod@alpha46.aulas.gsyc.urjc.es>
2017-11-20 14:19:37 +0100

    Hecho el envio de RTP en server.py

--- server.py
+++ server.py
@@ -6,13 +6,19 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import os
 
 try:
     IP = sys.argv[1]
     PORT = int(sys.argv[2])
-    FILE = sys.argv[3]
-except: 
+    FILE = sys.argv[3] 
+except:
     sys.exit("Usage: python3 server.py IP port audio_file")
+
+if not os.path.exists(FILE):
+    sys.exit("Document does not exist")
+      
+
 
 TRYING = b"SIP/2.0 100 Trying\r\n"
 RINGING = b"SIP/2.0 180 Ringing\r\n"
@@ -26,20 +32,23 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
 
     def handle(self):
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            doc = line.decode('utf-8').split(" ")
-            METHOD = doc[0]
-            if METHOD == "INVITE":
-                self.wfile.write(TRYING + RINGING + OK)
-            elif METHOD == "BYE":
-                self.wfile.write(OK)
-            elif METHOD != "ACK":
-                self.wfile.write(Not_Allowed)
-            if not line:
-                break
-            
+        # Leyendo línea a línea lo que nos envía el cliente
+        line = self.rfile.read()
+        doc = line.decode('utf-8').split(" ")
+        METHOD = doc[0]
+        if METHOD == "INVITE":
+            print("INVITE RECIVED")
+            self.wfile.write(TRYING + RINGING + OK)
+            aEjecutar = 'chmod +x mp32rtp & mp32rtp -i 127.0.0.1 -p 23032 <' + FILE
+            os.system(aEjecutar)
+        elif METHOD == "BYE":
+            print("BYE RECIVED")
+            self.wfile.write(OK)
+        elif METHOD == "ACK":
+            pass
+        else:
+            self.wfile.write(Not_Allowed)
+        
         # Si no hay más líneas salimos del bucle infinito
 
 if __name__ == "__main__":
@@ -50,3 +59,4 @@ if __name__ == "__main__":
         serv.serve_forever()
     except KeyboardInterrupt:
         print("Finalizado servidor")
+        
