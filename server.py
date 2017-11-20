6d5d4cec6dda0cab460073aa3a88ecc8d7ac62fd
David Aguado Dianez <daguadod@gamma18.aulas.gsyc.urjc.es>
2017-11-20 11:02:20 +0100

    Hecho hasta parte servidora en server.py

--- server.py
+++ server.py
@@ -5,7 +5,20 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
+try:
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+    FILE = sys.argv[3]
+except: 
+    sys.exit("Usage: python3 server.py IP port audio_file")
+
+TRYING = b"SIP/2.0 100 Trying\r\n"
+RINGING = b"SIP/2.0 180 Ringing\r\n"
+OK = b"SIP/2.0 200 OK\r\n"
+BAD = b"SIP/2.0 400 Bad Request\r\n"
+Not_Allowed =b"SIP/2.0 405 Method Not Allowed\r\n"
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -13,19 +26,27 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-
-            # Si no hay más líneas salimos del bucle infinito
+            doc = line.decode('utf-8').split(" ")
+            METHOD = doc[0]
+            if METHOD == "INVITE":
+                self.wfile.write(TRYING + RINGING + OK)
+            elif METHOD == "BYE":
+                self.wfile.write(OK)
+            elif METHOD != "ACK":
+                self.wfile.write(Not_Allowed)
             if not line:
                 break
+            
+        # Si no hay más líneas salimos del bucle infinito
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    serv = socketserver.UDPServer(('', PORT), EchoHandler)
+    print("Listening...")
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Finalizado servidor")
